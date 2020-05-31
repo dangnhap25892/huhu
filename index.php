0974f7bbeb33b2cd0f994b7ebc7d1e88ed2f201f
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-01 00:35:53 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+require './vendor/autoload.php';
+
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Drivers\DriverManager;
+
+$config = [
+   'facebook' => [
+  	'token' => 'EAANOvZBXEOzwBAPgubi86sTE5CqhlcqIsFxGZBmIiTtneJxaZAiGa5LKl2eJFWjSroZBPZB1G7ysbVzJZACuYZCqO3yFciCBVfQwIArKUaxGHSEs4eZCDOLIv7sQgZCO0AU1LkdVsE0DmcZC6mcvxuWH0ZAgD3hGdfDupc6zNEjeZCmTmQZDZD',
+	'app_secret' => '5bd19fb4829097baac701d0206483c3f',
+    'verification'=>'abc_123',
+]
+];
+
+// Load the driver(s) you want to use
+DriverManager::loadDriver(\BotMan\Drivers\Facebook\FacebookDriver::class);
+
+// Create an instance
+$botman = BotManFactory::create($config);
+
+// Give the bot something to listen for.
+$botman->hears('hello', function (BotMan $bot) {
+    $bot->reply('Hello yourself.');
+});
+
+// Start listening
+$botman->listen();
